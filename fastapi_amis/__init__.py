7e7bb92e00b1502b5163b10e806118f35847ca67
kylin <260987762@qq.com>
2025-10-03 13:37:10 +0800

    fix: add package metadata for PyPI publication

--- fastapi_amis/__init__.py
+++ fastapi_amis/__init__.py
@@ -1,1 +1,7 @@
+"""FastAPI Amis - A FastAPI integration with Baidu Amis."""
 
+__version__ = "0.0.1"
+__author__ = "kylin"
+__email__ = "260987762@qq.com"
+
+__all__ = ["__version__", "__author__", "__email__"]
